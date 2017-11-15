9a94a1af3bbf01eaa0a808e2bd04e495c4ed1c20
Joshua Kenney <joshuak@vmware.com>
2017-11-15 16:44:37 -0600

    e2e tests for vch create wizard

--- h5c/vic/src/vic-webapp/e2e/app.e2e-spec.ts
+++ h5c/vic/src/vic-webapp/e2e/app.e2e-spec.ts
@@ -15,16 +15,73 @@
 */
 
 import { VicWebappPage } from './app.po';
+import { browser, by, until, element } from 'protractor';
+import fs = require('fs');
+import path = require('path');
 
-describe('vic-webapp App', () => {
+describe('vic-webapp create vch', () => {
   let page: VicWebappPage;
+  const timeout = 15000;
 
   beforeEach(() => {
     page = new VicWebappPage();
   });
 
-  it('should display welcome message', () => {
+  it('should redirect to login', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(browser.getCurrentUrl()).toContain('SSO');
   });
+
+  it('should login', () => {
+    page.login();
+    page.takeScreenShot('wait_on_login');
+    browser.waitForAngularEnabled(true);
+    expect(browser.getCurrentUrl()).toContain('/ui');
+  });
+
+  it('should navigate to vsphere home', () => {
+    page.navigateToHome();
+    expect(browser.getCurrentUrl()).toContain('vsphere');
+  });
+
+  it('should navigate to vic plugin', () => {
+    page.navigateToVicPlugin();
+    expect(browser.getCurrentUrl()).toContain('vic');
+  });
+
+  it('should navigate to summary tab', () => {
+    page.navigateToSummaryTab();
+    expect(browser.getCurrentUrl()).toContain('vic-root');
+  });
+
+  it('should navigate to vch tab', () => {
+    page.navigateToVchTab();
+    expect(browser.getCurrentUrl()).toContain('customtab-vch');
+  });
+
+  it('should open create vch wizard', () => {
+    page.openVchWizard();
+    expect(element(by.css('.clr-wizard-stepnav')).isPresent()).toBe(true);
+  });
+
+  it('should input vch name', () => {
+    page.inputVchName();
+  });
+
+  it('should complete general step', () => {
+    page.clickNextButton();
+    // check if we made it to compute capacity section
+    expect(element(by.css('section#compute-capacity')).isPresent()).toBe(true);
+  });
+
+  it('should select a compute resource', () => {
+    page.selectComputeResource();
+  });
+
+  it('should complete compute resource step', () => {
+    page.clickNextButton();
+    // check if we made it to storage capacity section
+    expect(element(by.css('section#storage-capacity')).isPresent()).toBe(true);
+  });
+
 });
--- h5c/vic/src/vic-webapp/e2e/app.po.ts
+++ h5c/vic/src/vic-webapp/e2e/app.po.ts
@@ -14,14 +14,82 @@
  limitations under the License.
 */
 
-import { browser, by, element } from 'protractor';
+import { browser, by, until, element } from 'protractor';
+import fs = require('fs');
+import path = require('path');
 
 export class VicWebappPage {
   navigateTo() {
-    return browser.get('/');
+    browser.waitForAngularEnabled(false);
+    return browser.get('https://localhost:9443');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  login() {
+    browser.waitForAngularEnabled(false);
+    // username
+    browser.driver.findElement(by.css('#username')).click();
+    browser.driver.findElement(by.css('#username')).clear();
+    browser.driver.findElement(by.css('#username')).sendKeys('administrator@vsphere.local');
+    // password
+    browser.driver.findElement(by.css('#password')).click();
+    browser.driver.findElement(by.css('#password')).clear();
+    browser.driver.findElement(by.css('#password')).sendKeys('Admin!23');
+    // submit
+    browser.driver.findElement(by.css('#submit')).click();
+  }
+
+  navigateToHome() {
+    // click top left vmware logo
+    browser.driver.findElement(by.css('span.clr-vmw-logo')).click();
+  }
+
+  navigateToVicPlugin() {
+    // click vic shortcut icon
+    browser.driver.findElement(by.css('span.com_vmware_vic-home-shortcut-icon')).click();
+  }
+
+  navigateToSummaryTab() {
+    // click vic link
+    browser.driver.findElement(by.css('span.com_vmware_vic-vic-root-icon')).click();
+  }
+
+  navigateToVchTab() {
+    // click vch tab
+    element(by.cssContainingText('a', 'Virtual Container Hosts')).click();
+  }
+
+  openVchWizard() {
+    browser.switchTo().frame(browser.driver.findElement(by.css('div.outer-tab-content iframe.sandbox-iframe')));
+    this.waitForElementToBePresent('button.new-vch');
+    browser.driver.findElement(by.css('button.new-vch')).click();
+    browser.switchTo().defaultContent();
+    browser.switchTo().frame(browser.driver.findElement(by.css('div.modal-body iframe.sandbox-iframe')));
+  }
+
+  inputVchName() {
+    browser.driver.findElement(by.css('#nameInput')).sendKeys('-77');
+  }
+
+  selectComputeResource() {
+    this.waitForElementToBePresent('button.cc-resource');
+    browser.driver.findElement(by.css('button.cc-resource')).click();
+  }
+
+  clickNextButton() {
+    element(by.cssContainingText('button', 'Next')).click();
+  }
+
+  waitForElementToBePresent(element) {
+    browser.wait(function () {
+    return browser.isElementPresent(by.css(element));
+    }, 60000);
+  };
+
+  takeScreenShot(name) {
+    browser.takeScreenshot().then(function(png) {
+      const file = path.resolve('./' + name + '.png');
+      fs.writeFileSync(file, png);
+      fs.unlinkSync(file);
+    });
   }
 }
