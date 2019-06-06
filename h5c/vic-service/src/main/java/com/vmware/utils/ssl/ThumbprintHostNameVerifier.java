6254d0769af4e09d22c80d55e358950da5021819
Mia ZHOU <meinaz@vmware.com>
2019-06-06 08:39:44 +0800

    fix certificate chain will not show vic plugin (#672)
    Signed-off-by: Meina Zhou <meinaz@vmware.com>

--- h5c/vic-service/src/main/java/com/vmware/utils/ssl/ThumbprintHostNameVerifier.java
+++ h5c/vic-service/src/main/java/com/vmware/utils/ssl/ThumbprintHostNameVerifier.java
@@ -10,23 +10,26 @@ import java.security.cert.X509Certificate;
 
 public class ThumbprintHostNameVerifier implements HostnameVerifier {
 
-   @Override
-   public boolean verify(String host, SSLSession session) {
-      try {
-         Certificate[] certificates = session.getPeerCertificates();
-         verify(host, (X509Certificate) certificates[0]);
-         return true;
-      } catch (SSLException e) {
-         return false;
-      }
-   }
+    @Override
+    public boolean verify(String host, SSLSession session) {
+        try {
+            Certificate[] certificates = session.getPeerCertificates();
+            verify(host, (X509Certificate) certificates[0]);
+            return true;
+        } catch (SSLException e) {
+            return false;
+        }
+    }
 
-   private void verify(String host, X509Certificate cert) throws SSLException {
-      try {
-         String thumbprint = ThumbprintTrustManager.getThumbprint(cert);
-         ThumbprintTrustManager.checkThumbprint(thumbprint);
-      } catch(CertificateException e){
-         throw new SSLException(e.getMessage());
-      }
-   }
+    private void verify(String host, X509Certificate cert) throws SSLException {
+        try {
+            String thumbprint = ThumbprintTrustManager.getThumbprint(cert);
+            boolean hasThumbprint = ThumbprintTrustManager.checkThumbprint(thumbprint);
+            if (!hasThumbprint) {
+                throw new SSLException("Server certificate chain is not trusted and thumbprint doesn't match");
+            }
+        } catch (CertificateException e) {
+            throw new SSLException(e.getMessage());
+        }
+    }
 }
